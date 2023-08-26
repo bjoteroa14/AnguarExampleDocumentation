06ba5eb5dcdfb45a0b0541e583d7811ddde4d1af
Bryan Otero <bjoteroa14@gmail.com>
2023-08-26 16:32:08 -0500

    ngFor on templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { HomeComponent} from './home/home.component'
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  /* templateUrl: './app.component.html', */
+  template: ` 
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.ico" alt="logo" aria-hidden="true" width="32px">
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>`,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Empleado';
+  title = 'Homes';
 }
